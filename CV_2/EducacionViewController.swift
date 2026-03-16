0b718265e15ac9feee137709022c1ef8540c2b7a
Luisafer1-star <luisafer21052005@gmail.com>
2026-03-15 19:44:35 -0600

    Add EducacionViewController for education section

---
+++ CV_2/EducacionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EducacionViewController.swift
+//  CV_2
+//
+//  Created by Luisa Fernanda Hernández Reyes on 15/03/26.
+//
+
+import UIKit
+
+class EducacionViewController:
+    UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Do any additional setup after loading the view
+    }
+}
+ 
