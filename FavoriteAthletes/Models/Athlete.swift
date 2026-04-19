a12116b247476a5fac764eac445d3d5cd62ac7ca
Luisafer1-star <luisafer21052005@gmail.com>
2026-04-19 15:16:36 -0600

    Create Athlete model with name, age, and sport

---
+++ FavoriteAthletes/Models/Athlete.swift
@@ -0,0 +1,14 @@
+//
+//  Athlete.swift
+//  FavoriteAthletes
+//
+//  Created by Luisa Fernanda Hernández Reyes on 15/04/26.
+//
+
+import UIKit
+
+struct Athlete {
+    var name: String
+    var age: Int
+    var sport: String
+}
