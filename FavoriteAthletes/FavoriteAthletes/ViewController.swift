640d47bad6212309da578f1d233a0c6d37e6de1c
Luisafer1-star <luisafer21052005@gmail.com>
2026-04-19 15:23:05 -0600

    Add initial ViewController implementation

---
+++ FavoriteAthletes/FavoriteAthletes/ViewController.swift
@@ -0,0 +1,18 @@
+/
+//  ViewController.swift
+//  FavoriteAthletes
+//
+//  Created by Luisa Fernanda Hernández Reyes on 15/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
